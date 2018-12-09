287690d698d1ee60f8c1b53c6295a1a219817bf0
Ry <ryanthecarl@gmail.com>
2018-12-08 22:09:23 -0800

    refactor

---
+++ docs/precache-manifest.37308908678cfec8171dace20c73a335.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "26f73fd5c861ad7cffa6",
+    "url": "/Foxy-DataDriven-UX/static/js/runtime~main.26f73fd5.js"
+  },
+  {
+    "revision": "9da632620dfa12d04de1",
+    "url": "/Foxy-DataDriven-UX/static/js/main.9da63262.chunk.js"
+  },
+  {
+    "revision": "de8b0391a6e4dd52c91b",
+    "url": "/Foxy-DataDriven-UX/static/js/1.de8b0391.chunk.js"
+  },
+  {
+    "revision": "9da632620dfa12d04de1",
+    "url": "/Foxy-DataDriven-UX/static/css/main.3e4c223b.chunk.css"
+  },
+  {
+    "revision": "de8b0391a6e4dd52c91b",
+    "url": "/Foxy-DataDriven-UX/static/css/1.92987935.chunk.css"
+  },
+  {
+    "revision": "8253e327411c195980e3c2484208cc82",
+    "url": "/Foxy-DataDriven-UX/index.html"
+  }
+];
